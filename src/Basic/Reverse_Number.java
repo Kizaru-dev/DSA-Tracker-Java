f99d8c4771caca203ed313244954249fa43ab813
PRIYANSHU SINGH <235023819+Kizaru-dev@users.noreply.github.com>
2025-12-28 21:27:12 +0530

    solve reverse the array . using temp variable and using while loop

--- src/Basic/Reverse_Number.java
+++ src/Basic/Reverse_Number.java
@@ -1,7 +1,24 @@
 package Basic;
 
+import java.util.Arrays;
+
 public class Reverse_Number {
     public static void main(String[] args) {
+        // Reverse the Number .
+        int[] nums = {1,2,3,4,5,6,7,8,9};
+        reverseNumber(nums);
+    }
+    static void reverseNumber(int[] nums){
+        int start = 0 ;
+        int end = nums.length -1 ;
 
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp ;
+            start++ ;
+            end-- ;
+        }
+        System.out.println(Arrays.toString(nums));
     }
 }
