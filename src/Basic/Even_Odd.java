356f7bde564a8dc258ef6b66456b2b709616adcc
PRIYANSHU SINGH <235023819+Kizaru-dev@users.noreply.github.com>
2025-12-28 20:44:21 +0530

    Solve First Question of Basic DSA . Even and Odd

--- src/Basic/Even_Odd.java
+++ src/Basic/Even_Odd.java
@@ -2,6 +2,22 @@ package Basic;
 
 public class Even_Odd {
     public static void main(String[] args) {
+        //  check even or odd .
+        int[] number = {1,2,3,4,5,6,7,8,9,0};
+        numberChecker(number);
 
+    }
+    static void numberChecker(int[] number){
+        int even = 0 ;
+        int odd = 0 ;
+        for (int j : number) {
+            if (j % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        System.out.println("The ToTal number of Even number in This array is : " + even);
+        System.out.println("The Total number of Odd number in This array is : " + odd);
     }
 }
