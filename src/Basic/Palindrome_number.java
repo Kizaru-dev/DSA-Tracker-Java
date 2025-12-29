5e0b91d61494d30756e985b1ecf79e225985ea01
PRIYANSHU SINGH <235023819+Kizaru-dev@users.noreply.github.com>
2025-12-29 15:39:02 +0530

    I have solve one more problem by myself palidrome number

--- src/Basic/Palindrome_number.java
+++ src/Basic/Palindrome_number.java
@@ -2,6 +2,28 @@ package Basic;
 
 public class Palindrome_number {
     public static void main(String[] args) {
+        int n = 501020105 ;
+        System.out.println(isPalindrome(n));
 
+    }
+    public static boolean isPalindrome(int n) {
+        // code here
+        int reserve = 0 ;
+        int clone = n ;
+
+        if(n < 9 && n > 0 ){
+            return true ;
+        }
+
+        while(clone != 0 ){
+            int remainder = clone % 10 ;
+            clone  /= 10 ;
+            reserve = (reserve * 10 ) + remainder ;
+        }
+
+        if(n == reserve){
+            return true ;
+        }
+        return false ;
     }
 }
