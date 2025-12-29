7ae6eedff2f2905c300811ca90f27841827613bb
PRIYANSHU SINGH <235023819+Kizaru-dev@users.noreply.github.com>
2025-12-29 14:49:55 +0530

    I have solve One more Basic Question Sum Digit

--- src/Basic/Sum_Digit.java
+++ src/Basic/Sum_Digit.java
@@ -2,6 +2,19 @@ package Basic;
 
 public class Sum_Digit {
     public static void main(String[] args) {
-
+        int n = 1231;
+        sumDigit(n);
+    }
+    static void sumDigit(int n){
+        if(n < 0 && n > 10){
+            System.out.println("The sum of This Digit is  = " +  n );
+        }
+        int result = 0 ;
+        while(n != 0){
+            int remainder = n % 10 ;
+            n /= 10 ;
+            result +=remainder ;
+        }
+        System.out.println("The sum of This Digit is = " +  result);
     }
 }
