ae50776f885cb9a50a70bd6aeee0e5b015685b0d
PRIYANSHU SINGH <235023819+Kizaru-dev@users.noreply.github.com>
2025-12-29 15:03:25 +0530

    I have solve the Count Digit Number problem

--- src/Basic/Count_Digit_Number.java
+++ src/Basic/Count_Digit_Number.java
@@ -2,6 +2,20 @@ package Basic;
 
 public class Count_Digit_Number {
     public static void main(String[] args) {
+        int n = 234233 ;
+        String result  = countDigitNumber(n) == -1 ? "You have negative number " : "The Total numbers of digit in This number are : ";
+        System.out.println(result + countDigitNumber(n));
 
+    }
+    static int countDigitNumber(int n ){
+        if(n <  0 ){
+            return -1 ;
+        }
+        int count = 0 ;
+        while(n != 0 ){
+            n/= 10  ;
+            count++ ;
+        }
+        return count ;
     }
 }
