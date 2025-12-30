ed21edfe962564036fe71e1c497aa9adccafb408
PRIYANSHU SINGH <235023819+Kizaru-dev@users.noreply.github.com>
2025-12-30 16:28:54 +0530

    I have solve average array question using iterate loop simply and easy

--- src/Arrays/Basic/Average_Array.java
+++ src/Arrays/Basic/Average_Array.java
@@ -1,4 +1,23 @@
 package Arrays.Basic;
 
 public class Average_Array {
+    public static void main(String[] args) {
+        //Input : arr[] = {1, 2, 3, 4, 5}
+        //Output : 3
+        //Sum of the elements is 1+2+3+4+5 = 15
+        //and total number of elements is 5.
+        //So average is 15/5 = 3
+
+        int[] arr = {1,2,3,4,5};
+        averageFinder(arr);
+    }
+    static void averageFinder(int[] arr){
+        int l = arr.length  ; // length = 5 ;
+        int sum = 0 ;  // local variable to add all the element .
+        for(int i = 0 ; i < arr.length ; i++){      // iterate the loop and add all the element
+            sum  += arr[i];
+        }
+        int result = sum / l  ; // divide the total with the length of the array.
+        System.out.println("The Average number of This Arrays = " + result); // print the result .
+    }
 }
