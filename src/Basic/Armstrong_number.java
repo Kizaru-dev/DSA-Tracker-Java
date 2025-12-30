a1a2ed90e4dc97d9c6f873bb7eecb8716bb523dd
PRIYANSHU SINGH <235023819+Kizaru-dev@users.noreply.github.com>
2025-12-30 14:29:27 +0530

    solve armstrong question by myself

--- src/Basic/Armstrong_number.java
+++ src/Basic/Armstrong_number.java
@@ -2,6 +2,24 @@ package Basic;
 
 public class Armstrong_number {
     public static void main(String[] args) {
+        int n = 153 ;
+        armstrongChecker(n);
+    }
+    static void armstrongChecker(int n){
+        int clone = n ;
+        int r = 0 ;
+        int result = 0 ;
 
+        while(clone != 0 ){
+            r = clone % 10 ;
+            clone /= 10 ;
+            result += (r*r*r);
+        }
+        if(n == result){
+            System.out.println("It's the Armstrong number");
+        }
+        else{
+            System.out.println("It's not a Armstrong number . ");
+        }
     }
 }
