4ed4f71dcb4e25b40906ad2c32461e4df14d12ea
PRIYANSHU SINGH <235023819+Kizaru-dev@users.noreply.github.com>
2025-12-30 16:58:31 +0530

    i have solve one more question and this one was sum of the element

--- src/Arrays/Basic/Sum_Element.java
+++ src/Arrays/Basic/Sum_Element.java
@@ -1,4 +1,23 @@
 package Arrays.Basic;
 
 public class Sum_Element {
+    public static void main(String[] args) {
+        /*
+        nput : arr[] = {1, 2, 3}
+        Output : 6
+        Explanation: 1 + 2 + 3 = 6
+
+        Input : arr[] = {15, 12, 13, 10}
+        Output : 50
+         */
+        int[] arr = {1,2,4};
+        sumElement(arr);
+    }
+    static void sumElement(int[] arr){
+        int result = 0 ;
+        for(int j : arr ){
+            result  += j ;
+        }
+        System.out.println("The sum of the element is " + result);
+    }
 }
