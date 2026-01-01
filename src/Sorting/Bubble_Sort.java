b373ad5d2a0a17a5f7216d73684f7d37550c5456
PRIYANSHU SINGH <235023819+Kizaru-dev@users.noreply.github.com>
2026-01-01 22:41:03 +0530

    understand how bubble sort work and how selelction sort work

--- src/Sorting/Bubble_Sort.java
+++ src/Sorting/Bubble_Sort.java
@@ -1,4 +1,23 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class Bubble_Sort {
+    public static void main(String[] args) {
+        int[] arr = {23,123,12,42,21,321,41};
+        bubbleSort(arr);
+    }
+    public static void bubbleSort(int[] arr){
+        int n = arr.length ;
+        for(int i = 0 ; i < arr.length ; i++){
+            for(int j = 0 ; j < n-1-i;j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp ;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
--- src/Sorting/Selection_Sort.java
+++ src/Sorting/Selection_Sort.java
@@ -1,4 +1,25 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class Selection_Sort {
+    public static void main(String[] args) {
+        int[] arr = {12,23,24,11,22,14};
+        selectionSort(arr);
+    }
+     static void selectionSort(int[] arr){
+      int n = arr.length ;  // lenght
+      for(int i =  0 ; i < arr.length - 1 ; i++){//iteration hai
+          int smallest= i ;  // smallest element
+          for(int j = i + 1  ; j < n ; j++){ // i + 1 to n ;
+              if(arr[smallest] > arr[j]){ // agar smallest se koi bat hai to sote kardo
+                  smallest  = j ;
+              }
+          }
+          int temp = arr[smallest] ; //swapping kardo aur change kardo .
+          arr[smallest]  = arr[i];
+          arr[i] = temp ;
+      }
+        System.out.println(Arrays.toString(arr));
+    }
 }
