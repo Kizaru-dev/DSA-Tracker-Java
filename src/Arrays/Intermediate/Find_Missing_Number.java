30d51e07c91e69fe56dfc44e108dedb8556fe749
PRIYANSHU SINGH <235023819+Kizaru-dev@users.noreply.github.com>
2026-01-01 14:54:17 +0530

    i have solve 2 question in intermates level move all the zero quesiton

--- src/Arrays/Intermediate/Find_Missing_Number.java
+++ src/Arrays/Intermediate/Find_Missing_Number.java
@@ -1,4 +1,22 @@
 package Arrays.Intermediate;
 
 public class Find_Missing_Number {
+    public static void main(String[] args) {
+        int[] arr = {21,22,23,24,25,26,27,28,30};
+        int n = arr.length + 1 ;
+
+        for(int i = 1 ; i <= n ; i++){
+            boolean found = false ;
+            for(int j = 0 ; j < n-1 ; j++){
+                if(arr[j] == i ){
+                    found = true ;
+                    break;
+                }
+            }
+
+            if(!found){
+                System.out.println("The number is " + i);
+            }
+        }
+    }
 }
--- src/Arrays/Intermediate/Move_All_Zeros_End.java
+++ src/Arrays/Intermediate/Move_All_Zeros_End.java
@@ -1,4 +1,23 @@
 package Arrays.Intermediate;
 
+import java.util.Arrays;
+
 public class Move_All_Zeros_End {
+    public static void main(String[] args) {
+        // Move all The Zero at the end of the array .
+        int[] arr = {0,1,0,2,0,3,0,4,0,5};
+        moveZero(arr);
+    }
+    static void moveZero(int[] arr){
+        int l = arr.length ;
+        int[] temp = new int [l];
+        int k = 0 ;
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] != 0 ){
+                temp[k] = arr[i];
+                k++;
+            }
+        }
+        System.out.println(Arrays.toString(temp));
+    }
 }
