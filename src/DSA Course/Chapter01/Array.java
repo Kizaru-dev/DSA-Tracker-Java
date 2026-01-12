b7f55b753389ecf4b698c8a772aef6143572a4e1
PRIYANSHU SINGH <235023819+Kizaru-dev@users.noreply.github.com>
2026-01-12 16:13:06 +0530

    Learn about array

--- src/DSA Course/Chapter01/Array.java
+++ src/DSA Course/Chapter01/Array.java
@@ -2,13 +2,17 @@
 
 public class Array {
   public static void main(String[] args) {
-    // How to Create an Array
-    int[] age = {21,22,23,24,25};
-    // How to access
-    System.out.println("The age of this index is. " + age[2]);
-
-    for(int i = 0 ; i < age.length ; i++){
-      System.out.println("This age is " + age[i]);
+  //   // How to Create an Array
+  //   int[] age = {21,22,23,24,25};
+  //   // How to access
+  //   System.out.println("The age of this index is. " + age[2]);
+  //   // for Each Loop
+  //   for(int i : age){
+  //     System.out.println(i);
+  //   }
+  int age[] = new int[5];
+    for(int i = 0 ; i < age.length ;i++){
+        System.out.println("Enter your ");
     }
   }
 
